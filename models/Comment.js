a9a32a92adb5b9b30de9c854fccd7b7a7980697a
danilbek <deni200040@gmail.com>
2021-06-03 14:21:01 +0300

    finish

---
+++ models/Comment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Comment = new mongoose.Schema({  
+    firstName: {type: String, required: true},
+    text: {type: String, required: true},
+    post: {type:  mongoose.Schema.Types.ObjectId, ref: "post"},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: { type: Date, default: Date.now },
+})
+
+module.exports = mongoose.model('comment', Comment)
